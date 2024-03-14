fee9e091a14e6584761f31622f15e88d8d9ca117
Ribich1 <ribich@ukr.net>
2024-03-14 22:29:08 +0100

    timerx3  + hours

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Stopwatch } from './Stopwatch';
+
 export const App = () => {
+  const time1 = Stopwatch(60);
+  const time2 = Stopwatch(90);
+  const time3 = Stopwatch(7400);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Stopwatch
+    <div>
+      {time1}
+      {time2}
+      {time3}
     </div>
   );
 };
