cf6314ad45d4be83314d5e3340583f9984236be2
Ribich1 <ribich@ukr.net>
2024-03-29 21:26:35 +0100

    radio button choose

--- src/components/TimerForm.jsx
+++ src/components/TimerForm.jsx
@@ -14,32 +14,58 @@ export const TimerForm = () => {
   };
   const onSubmit = (values, { resetForm }) => {
     const id = nanoid();
-      const timer = timerMs(values.timer);
-      if (values.variant === 'Two') {
-          console.log("two");
-          return
-      }
+    const timer = timerMs(values.timer);
+    if (values.variant === 'Two') {
+      console.log('two');
+      return;
+    }
     addTimer({ ...values, timer, id });
     resetForm();
   };
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      <Form>
-        <label htmlFor="name">Name of Timer</label>
-        <Field type="text" id="name" name="name" placeholder="Name of Timer" />
-        <label htmlFor="timer">Duration of Timer</label>
-        <Field type="time" id="timer" name="timer" step="1" />
-        <button type="submit">Submit</button>
-        <label>
-          <Field type="radio" name="variant" value="One" />
-          One
-        </label>
-        <label>
-          <Field type="radio" name="variant" value="Two" />
-          Two
-        </label>
-      </Form>
+      {({ values }) => (
+        <Form>
+          <label>
+            <Field type="radio" name="variant" value="One" />
+            One
+          </label>
+          {values.variant === 'One' && (
+            <>
+              <label htmlFor="name">Name of Timer</label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name of Timer"
+              />
+              <label htmlFor="timer">Duration of Timer</label>
+              <Field type="time" id="timer" name="timer" step="1" />
+            </>
+          )}
+          <label>
+            <Field type="radio" name="variant" value="Two" />
+            Two
+          </label>
+          {values.variant === 'Two' && (
+            <>
+              <label htmlFor="nameUntil">Name of Timer</label>
+              <Field
+                type="text"
+                id="nameUntil"
+                name="nameUntil"
+                placeholder="Name of Timer"
+              />
+              <label htmlFor="date">Timer until the date:</label>
+              <Field type="date" id="date" name="date" />
+              <label htmlFor="timeUntil">Timer until the time:</label>
+              <Field type="time" id="timeUntil" name="timeUntil" />
+            </>
+          )}
+          <button type="submit">Add Timer</button>
+        </Form>
+      )}
     </Formik>
   );
 };
