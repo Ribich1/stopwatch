1dcc10fb8f31166686c0adb451fd2c201f1640bf
Ribich1 <ribich@ukr.net>
2024-04-05 23:38:25 +0200

    add local storage

--- src/context/ContextTimers.jsx
+++ src/context/ContextTimers.jsx
@@ -3,18 +3,21 @@ import { useState, useContext, createContext } from 'react';
 const timersContext = createContext(null);
 
 export const useTimers = () => useContext(timersContext);
-
+const local = JSON.parse(localStorage.getItem('timers'));
 export default function ContextProvider({ children }) {
-  const [timers, setTimers] = useState([]);
+  const [timers, setTimers] = useState([...local]);
+  
+  console.log(JSON.parse(localStorage.getItem('timers')));
 
     const addTimer = (timer) => {
+      
       setTimers(prev => [...prev, timer]);
     };
     
     const delTimer = id => {
       setTimers(timers.filter(a => a.id !== id));
     };
-
+localStorage.setItem('timers', JSON.stringify(timers));
   const initialValues = { timers, addTimer, delTimer };
   return (
     <timersContext.Provider value={initialValues}>
